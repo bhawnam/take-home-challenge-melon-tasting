f06ec1368aab9c9365d94b84a4cbd1af37344cf7
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-08-28 16:27:58 -0700

    Setup a server page to include all the routes for the app.

---
+++ server.py
@@ -0,0 +1,21 @@
+"""Server for melon reservations app."""
+
+from flask import (Flask, render_template, redirect, request, session, flash)
+from model import connect_to_db
+import crud
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "takehomechallenge"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def homepage():
+    """View homepage. """
+
+    return render_template("homepage.html")
+
+
+if __name__=='__main__':
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
