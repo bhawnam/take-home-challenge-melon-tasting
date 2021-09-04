edee8454bbbfff1c8415150c46397f94d214cba8
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-09-03 18:06:51 -0700

    Added a /login route in the server to allow user to login. Also, included a form in the himepage template to let the user enter thier credentials.

--- server.py
+++ server.py
@@ -16,6 +16,13 @@ def homepage():
     return render_template("homepage.html")
 
 
+@app.route('/login', methods=["POST"])
+def login():
+    """View login page. """
+
+    return render_template("login.html")
+
+
 if __name__=='__main__':
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
